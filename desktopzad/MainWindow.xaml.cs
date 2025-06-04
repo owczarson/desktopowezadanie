e64b9dc76bc98f1ed7909fb635e0b5151e39ccfa
Admin <Admin@OWCZAREK>
2025-06-04 20:22:24 +0200

    Dodaj pliki projektów.

---
+++ desktopzad/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using DodajPracownika;    
+
+
+namespace desktopzad
+{
+    public partial class MainWindow : Window
+    {
+        Pracownik pracownik = new Pracownik();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerujHaslo_Click(object sender, RoutedEventArgs e)
+        {
+            int dlugosc = int.TryParse(txtIloscZnakow.Text, out int wynik) ? wynik : 5;
+            bool litery = chkLitery.IsChecked == true;
+            bool cyfry = chkCyfry.IsChecked == true;
+            bool znaki = chkZnaki.IsChecked == true;
+
+            string haslo = pracownik.GenerujHaslo(dlugosc, litery, cyfry, znaki);
+            MessageBox.Show(haslo);
+        }
+
+        private void Zatwierdz_Click(object sender, RoutedEventArgs e)
+        {
+            pracownik.Imie = txtImie.Text;
+            pracownik.Nazwisko = txtNazwisko.Text;
+            pracownik.Stanowisko = (cmbStanowisko.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+            MessageBox.Show(pracownik.ToString());
+        }
+    }
+}
---
+++ desktopzad/Pracownik.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace DodajPracownika
+{
+    public class Pracownik
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Stanowisko { get; set; }
+        public string Haslo { get; set; }
+
+        private static readonly string maleLitery = "abcdefghijklmnopqrstuvwxyz";
+        private static readonly string wielkieLitery = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static readonly string cyfry = "0123456789";
+        private static readonly string znakiSpecjalne = "!@#$%^&*()_+-=";
+
+        public string GenerujHaslo(int dlugosc, bool czyLitery, bool czyCyfry, bool czyZnakiSpecjalne)
+        {
+            StringBuilder zbior = new StringBuilder();
+            string haslo = "";
+
+            if (czyLitery) zbior.Append(maleLitery + wielkieLitery);
+            else zbior.Append(maleLitery);
+
+            Random rnd = new Random();
+
+            if (dlugosc >= 1 && czyCyfry)
+                haslo += cyfry[rnd.Next(cyfry.Length)];
+
+            if (dlugosc >= 2 && czyZnakiSpecjalne)
+                haslo += znakiSpecjalne[rnd.Next(znakiSpecjalne.Length)];
+
+            for (int i = haslo.Length; i < dlugosc; i++)
+                haslo += zbior[rnd.Next(zbior.Length)];
+
+            Haslo = new string(haslo.ToCharArray());
+            return Haslo;
+        }
+
+        public override string ToString()
+        {
+            return $"Dane pracownika: {Imie} {Nazwisko} {Stanowisko} Hasło: {Haslo}";
+        }
+    }
+}
